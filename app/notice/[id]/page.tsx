4d9f7dc5d8ef04dad5f4c814f153f151230e3b62
Coalery <doralife12@gmail.com>
2024-03-27 00:22:24 +0900

    feat: 공지 페이지 구현

--- app/notice/[id]/page.tsx
+++ app/notice/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { fetchNotice } from "@khlug/util/fetchNotice";
 import { formatDate } from "@khlug/util/formaDate";
 import { getDateDiffText } from "@khlug/util/getDateDiffText";
 
@@ -7,41 +8,26 @@ type NoticeDetailPageProps = {
   };
 };
 
-export default function NoticeDetailPage({ params }: NoticeDetailPageProps) {
-  const { id } = params;
+export default async function NoticeDetailPage({
+  params,
+}: NoticeDetailPageProps) {
+  const { id: noticeId } = params;
+  const notice = await fetchNotice(noticeId);
 
-  const mockDocument = {
-    id: "1",
-    title: "khuthon 2023 접수가 마감되었습니다!",
-    content: [
-      "<p>기존 공지와 같이 참가 신청 기간이 9일 목요일 23:59까지로 연장되었으나</p>",
-      "<p>참가 제한 인원인 128명에 도달하여 11월 7일 17:57:27 부로 접수가 마감되었습니다.</p>",
-      "<p>38팀 128명이 참가하는 이번 대회는 D-3에 공개된 '교육의 정보화'라는 주제와 연계해 자유롭게 작품의 주제를 선정해 무박 2일간 해커톤을 벌이게 됩니다.</p>",
-      "<p>khuthon 2023에 많은 관심 가져주셔서 진심으로 고맙습니다.</p>",
-      "<p> </p>",
-      "<p>오는 11월 10일 금요일 18시에,</p>",
-      "<p>전자정보대학관 205호에서 뵙겠습니다!</p>",
-    ].join(""),
-    files: [{ id: "fileId", url: "fileUrl", name: "파일.pdf" }],
-    createdAt: new Date(2023, 10, 7, 18, 11, 25),
-    updatedAt: new Date(),
-  };
-
-  const isUpdated =
-    mockDocument.createdAt.getTime() !== mockDocument.updatedAt.getTime();
+  const isUpdated = notice.createdAt.getTime() !== notice.updatedAt.getTime();
 
   return (
     <div className="document">
       <div className="document_header">
-        <h3 className="title">{mockDocument.title}</h3>
+        <h3 className="title">{notice.title}</h3>
         <div className="created_at">
-          {formatDate(mockDocument.createdAt)} (
-          {getDateDiffText(mockDocument.createdAt, new Date())})
+          {formatDate(notice.createdAt)} (
+          {getDateDiffText(notice.createdAt, new Date())})
           {isUpdated && (
             <>
               <br />
-              {formatDate(mockDocument.updatedAt)} (
-              {getDateDiffText(mockDocument.updatedAt, new Date())}) 수정{" "}
+              {formatDate(notice.updatedAt)} (
+              {getDateDiffText(notice.updatedAt, new Date())}) 수정{" "}
             </>
           )}
         </div>
@@ -49,9 +35,10 @@ export default function NoticeDetailPage({ params }: NoticeDetailPageProps) {
       <div className="document_body">
         <div
           className="real_content"
-          dangerouslySetInnerHTML={{ __html: mockDocument.content }}
+          dangerouslySetInnerHTML={{ __html: notice.content }}
         ></div>
-        {mockDocument.files.length > 0 && (
+        {/* TODO[lery]: 파일 관련 코드 추가 필요 */}
+        {/* {mockDocument.files.length > 0 && (
           <div className="download">
             {mockDocument.files.map((file) => (
               <p key={file.id}>
@@ -62,7 +49,7 @@ export default function NoticeDetailPage({ params }: NoticeDetailPageProps) {
               </p>
             ))}
           </div>
-        )}
+        )} */}
       </div>
 
       <div className="btnArea">
---
+++ util/fetchNotice.ts
@@ -0,0 +1,19 @@
+type Notice = {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export async function fetchNotice(noticeId: string): Promise<Notice> {
+  const eventCdnPath = `https://cdn.khlug.org/notices/${noticeId}.json`;
+  const response = await fetch(eventCdnPath);
+  const notice: Notice = await response.json();
+
+  return {
+    ...notice,
+    createdAt: new Date(notice.createdAt),
+    updatedAt: new Date(notice.updatedAt),
+  };
+}
