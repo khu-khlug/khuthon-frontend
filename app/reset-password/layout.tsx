54563a99d94e6e153d460a2af3be35363e6104ab
Coalery <doralife12@gmail.com>
2024-04-08 01:33:08 +0900

    fix: suspense 추가

--- app/reset-password/layout.tsx
+++ app/reset-password/layout.tsx
@@ -1,9 +1,14 @@
 import ClientProvider from "@khlug/components/ClientProvider/ClientProvider";
+import { Suspense } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function TeamLayout({ children }: Props) {
-  return <ClientProvider>{children}</ClientProvider>;
+  return (
+    <ClientProvider>
+      <Suspense>{children}</Suspense>
+    </ClientProvider>
+  );
 }
