da482305a1c607bdaa398673590127c648fc22f9
Coalery <doralife12@gmail.com>
2024-04-08 01:10:56 +0900

    fix: 디버깅용 코드 제거

--- util/createEvent.ts
+++ util/createEvent.ts
@@ -16,29 +16,29 @@ export async function createEvent(): Promise<Event> {
     res.json()
   );
 
-  // const registerStartAt = new Date(eventJson.registerStartAt);
-  // const registerEndAt = new Date(eventJson.registerEndAt);
-  // const registerRange = calcTimeRange(registerStartAt, registerEndAt);
-
-  // const eventStartAt = new Date(eventJson.eventStartAt);
-  // const eventEndAt = new Date(eventJson.eventEndAt);
-  // const eventRange = calcTimeRange(eventStartAt, eventEndAt);
-
-  // const judgeStartAt = new Date(eventJson.judgeStartAt);
-  // const judgeEndAt = new Date(eventJson.judgeEndAt);
-  // const judgeRange = calcTimeRange(judgeStartAt, judgeEndAt);
-
-  const registerStartAt = new Date(2024, 0, 1, 0, 0, 0);
-  const registerEndAt = new Date(2024, 11, 31, 0, 0, 0);
+  const registerStartAt = new Date(eventJson.registerStartAt);
+  const registerEndAt = new Date(eventJson.registerEndAt);
   const registerRange = calcTimeRange(registerStartAt, registerEndAt);
 
-  const eventStartAt = new Date(2024, 0, 1, 0, 0, 0);
-  const eventEndAt = new Date(2024, 11, 31, 0, 0, 0);
+  const eventStartAt = new Date(eventJson.eventStartAt);
+  const eventEndAt = new Date(eventJson.eventEndAt);
   const eventRange = calcTimeRange(eventStartAt, eventEndAt);
 
-  const judgeStartAt = new Date(2024, 2, 31, 0, 0, 0);
-  const judgeEndAt = new Date(2024, 11, 31, 0, 0, 0);
+  const judgeStartAt = new Date(eventJson.judgeStartAt);
+  const judgeEndAt = new Date(eventJson.judgeEndAt);
   const judgeRange = calcTimeRange(judgeStartAt, judgeEndAt);
+
+  // const registerStartAt = new Date(2024, 0, 1, 0, 0, 0);
+  // const registerEndAt = new Date(2024, 11, 31, 0, 0, 0);
+  // const registerRange = calcTimeRange(registerStartAt, registerEndAt);
+
+  // const eventStartAt = new Date(2024, 0, 1, 0, 0, 0);
+  // const eventEndAt = new Date(2024, 11, 31, 0, 0, 0);
+  // const eventRange = calcTimeRange(eventStartAt, eventEndAt);
+
+  // const judgeStartAt = new Date(2024, 2, 31, 0, 0, 0);
+  // const judgeEndAt = new Date(2024, 11, 31, 0, 0, 0);
+  // const judgeRange = calcTimeRange(judgeStartAt, judgeEndAt);
 
   return {
     registerStartAt,
