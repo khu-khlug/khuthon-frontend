d7a0f8c8085616002f54acdffb6de4d4f45fd6ab
Coalery <doralife12@gmail.com>
2024-04-10 02:15:21 +0900

    fix: 유효하지 않은 토큰인 경우 리셋하는 코드를 use effect 안으로 이동

--- app/judge/page.tsx
+++ app/judge/page.tsx
@@ -4,13 +4,16 @@ import { useToken } from "@khlug/components/ClientProvider/ClientProvider";
 import Judge from "@khlug/components/judge/Judge/Judge";
 import JudgeLoginForm from "@khlug/components/judge/JudgeLoginForm/JudgeLoginForm";
 import { isTokenFor } from "@khlug/util/isTokenFor";
+import { useEffect } from "react";
 
 export default function JudgePage() {
   const [token, setToken] = useToken();
 
-  if (token && !isTokenFor(token, "EXAMINER")) {
-    setToken(null);
-  }
+  useEffect(() => {
+    if (token && !isTokenFor(token, "EXAMINER")) {
+      setToken(null);
+    }
+  }, [token, setToken]);
 
   return token ? <Judge /> : <JudgeLoginForm />;
 }
--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import { useToken } from "@khlug/components/ClientProvider/ClientProvider";
-import { NavItem } from "@khlug/components/Header/NavItem";
 import ManagerLoginContainer from "@khlug/components/manager/ManagerLoginContainer/ManagerLoginContainer";
-import MemberListContainer from "@khlug/components/manager/MemberListContainer/MemberListContainer";
 import { isTokenFor } from "@khlug/util/isTokenFor";
+import { useEffect } from "react";
 
 export default function ManagerPage() {
   const [token, setToken] = useToken();
 
-  if (token && !isTokenFor(token, "MANAGER")) {
-    setToken(null);
-  }
+  useEffect(() => {
+    if (token && !isTokenFor(token, "MANAGER")) {
+      setToken(null);
+    }
+  }, [token, setToken]);
 
   return token ? (
     <>
