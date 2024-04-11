fb46a0438f259bb0aab8ffe2623a4161d09d84ec
Coalery <doralife12@gmail.com>
2024-04-11 22:21:27 +0900

    feat: 채널톡 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,15 @@ export default function RootLayout({ children }: Props) {
         <Header />
         <div id="body">{children}</div>
         <Footer />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
+            ChannelIO('boot', {
+              "pluginKey": "be159df3-fbbc-4c1e-a2f3-8ccaa7bf1b7b"
+            });`,
+          }}
+        />
       </body>
     </html>
   );
