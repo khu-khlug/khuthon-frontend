46cf2e4496eca44cb0c459b0bc8374bb3fce8eca
Coalery <doralife12@gmail.com>
2024-04-14 23:57:21 +0900

    feat: 홈 페이지 보안 안내 추가

--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Callout from "@khlug/components/Callout/Callout";
 import { useToken } from "@khlug/components/ClientProvider/ClientProvider";
+import Container from "@khlug/components/Container/Container";
 import ManagerLoginContainer from "@khlug/components/manager/ManagerLoginContainer/ManagerLoginContainer";
 import { isTokenFor } from "@khlug/util/isTokenFor";
 import { useEffect } from "react";
@@ -16,7 +18,17 @@ export default function ManagerPage() {
 
   return token ? (
     <>
-      <div>안녕!</div>
+      <Container>
+        <Callout>
+          <span className="text-2xl font-bold">
+            본 페이지는 보안등급 2급으로 운영진 및 관계자만 볼 수 있습니다.
+          </span>
+          <br />
+          <span className="text-xl">
+            보안에 각별히 신경을 써주시기 바랍니다.
+          </span>
+        </Callout>
+      </Container>
     </>
   ) : (
     <ManagerLoginContainer />
