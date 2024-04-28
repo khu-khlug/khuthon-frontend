b71b34685121f28852637e821c5955b09d42d247
Coalery <doralife12@gmail.com>
2024-04-29 01:34:48 +0900

    feat: 이메일 인증 재시도 구현

--- components/TextButton/index.tsx
+++ components/TextButton/index.tsx
@@ -2,18 +2,31 @@ import classNames from "classnames";
 
 type Props = {
   onClick?: () => void;
+  loading?: boolean;
   className?: string;
   children?: React.ReactNode;
 };
 
-export default function TextButton({ onClick, className, children }: Props) {
+export default function TextButton({
+  onClick,
+  loading,
+  className,
+  children,
+}: Props) {
+  const handleClick = () => {
+    if (loading || !onClick) return;
+    onClick();
+  };
+
   return (
     <button
+      type="button"
       className={classNames(
-        "text-black hover:text-black/70 underline underline-offset-2 cursor-pointer border-0 bg-none bg-transparent text-base p-0",
+        "text-black hover:text-black/70 underline underline-offset-2 cursor-pointer border-0 bg-none bg-transparent p-0",
+        { "cursor-wait": loading },
         className
       )}
-      onClick={onClick}
+      onClick={handleClick}
     >
       {children}
     </button>
--- components/register/EmailOtpVerifyForm/EmailOtpVerifyForm.tsx
+++ components/register/EmailOtpVerifyForm/EmailOtpVerifyForm.tsx
@@ -2,17 +2,23 @@
 
 import { useState } from "react";
 import { useRegister } from "../MemberRegisterInfoProvider/MemberRegisterInfoProvider";
-import { useClient } from "@khlug/components/ClientProvider/ClientProvider";
+import {
+  useClient,
+  useToken,
+} from "@khlug/components/ClientProvider/ClientProvider";
 import { extractErrorMessage } from "@khlug/util/getErrorMessageFromAxiosError";
 import Button from "@khlug/components/Button";
+import TextButton from "@khlug/components/TextButton";
 
 export default function EmailOtpVerifyForm() {
   const [memberRegisterInfo, load] = useRegister();
   const client = useClient();
+  const [, setToken] = useToken();
 
   const [message, setMessage] = useState<string | null>(null);
   const [otp, setOtp] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
+  const [retryLoading, setRetryLoading] = useState<boolean>(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -30,16 +36,54 @@ export default function EmailOtpVerifyForm() {
     setLoading(false);
   };
 
+  const retryOtp = async () => {
+    setMessage(null);
+    setRetryLoading(true);
+    if (retryLoading) return;
+
+    try {
+      await client.post("/members/verify-email");
+    } catch (e) {
+      setMessage(extractErrorMessage(e));
+    }
+
+    setRetryLoading(false);
+  };
+
+  const reset = async () => {
+    setToken(null, { persist: true });
+    location.reload();
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       {message && <div className="error">{message}</div>}
 
       <label>참가자 정보 입력</label>
       <div className="description">
-        입력하신 이메일로 도착한 인증코드를 입력해주세요.
+        입력하신 이메일로 도착한 인증코드를 입력해주세요. 인증코드는 5분 동안
+        유효합니다.
         <br />
-        이메일을 찾을 수 없는 스팸 메일함을 확인해주세요. 어디에서도 인증코드를
-        담은 이메일을 찾을 수 없다면 관리자에게 연락해주세요.
+        인증코드가 도착하지 않았거나 인증코드의 유효 기간이 지났다면{" "}
+        <TextButton
+          className="text-sm"
+          onClick={retryOtp}
+          loading={retryLoading}
+        >
+          여기
+        </TextButton>
+        를 눌러 재시도해주세요.
+        <br />
+        잘못된 이메일을 입력해서, 처음부터 다시 진행하고 싶으시다면{" "}
+        <TextButton onClick={reset} className="text-sm">
+          여기
+        </TextButton>
+        를 눌러주세요.
+        <br />
+        <br />
+        이메일을 찾을 수 없다면 스팸 메일함을 확인해주세요. 어디에서도
+        인증코드를 담은 이메일을 찾을 수 없거나 지속적으로 문제를 겪고 계시다면
+        관리자에게 연락해주세요.
       </div>
       <div className="input_wrap">
         <input type="email" value={memberRegisterInfo?.email} readOnly />
@@ -48,7 +92,7 @@ export default function EmailOtpVerifyForm() {
         <input
           type="text"
           value={otp}
-          onChange={(e) => setOtp(e.target.value)}
+          onChange={(e) => setOtp(e.target.value.toUpperCase())}
           placeholder="OTP"
           required
         />
