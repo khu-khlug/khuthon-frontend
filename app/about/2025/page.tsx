33056e8a63baf8fc1d42a1582ebd9cb8b2e0c3c7
Coalery <doralife12@gmail.com>
2025-04-14 01:29:38 +0900

    feat: 2025년 대회 소개 페이지 추가

---
+++ app/about/2025/page.tsx
@@ -0,0 +1,260 @@
+export default function _2025About() {
+  return (
+    <>
+      <div className="container about">
+        <h4>일시</h4>
+        <p>
+          <strong>
+            2025년 5월 9일 금요일 18:00 ~ 2025년 5월 10일 토요일 12:30 (무박
+            2일)
+          </strong>
+        </p>
+
+        <h4>장소</h4>
+        <p>경희대학교 국제캠퍼스 전자정보대학관 205호, 211-1호, 211-2호</p>
+
+        <h4>주제</h4>
+        <ul className="!m-0">
+          <li className="text-gray-500">D-1 오후 12시에 공개</li>
+        </ul>
+        <p>
+          다양한 분야로 도전할 수 있습니다.
+          <br />
+          퀄리티가 좋지 않아도 괜찮습니다. 재미있게 만들고 즐겁게 공유해봐요!
+        </p>
+
+        <h4>참가 접수</h4>
+        <p>
+          <strong>
+            2025년 4월 28일 월요일 00:00 ~ 2025년 5월 5일 월요일 23:59
+          </strong>
+        </p>
+        <p>
+          <a href="https://thon.khlug.org/">https://thon.khlug.org/</a>에서 팀
+          단위로 참가 접수
+          <br />
+          선착순으로 48팀 또는 140명까지만 참가 신청을 받습니다.
+          <br />한 팀은 최소 1명부터 최대 4명으로 구성되며, 모든 팀원은 반드시
+          재학생이어야 합니다.
+        </p>
+
+        <h4>접수 과정</h4>
+        <ol className="!ml-0">
+          <li>참가자의 정보를 입력하여 계정을 생성한다.</li>
+          <li>
+            받은 초대가 없다면 팀을 생성하거나 다른 팀원의 초대를 기다린다.
+            <ul className="!ml-0">
+              <li>
+                팀을 생성했다면 팀원의 학번을 모두 입력하여 팀에 초대한다.
+              </li>
+              <li>받은 초대가 있다면 자동으로 수락된다.</li>
+            </ul>
+          </li>
+          <li>
+            초대한 팀원이 모두 수락하면, <b>인원 확정</b>을 할 수 있다.
+            <ul className="!ml-0">
+              <li>
+                인원 확정을 통해 팀의 참가 등록이 완료되며, 인원 확정을 하지
+                않은 팀은 <b>대회에 참가할 수 없다.</b>
+              </li>
+              <li>
+                모든 팀원이 초대를 수락하더라도, 인원 확정 시점에 최대 참가자 수
+                혹은 최대 팀 수를 초과했다면 대회에 참가할 수 없다.
+              </li>
+            </ul>
+          </li>
+        </ol>
+
+        <h4>수상</h4>
+        <div>
+          <h5>대상 (1팀)</h5>
+          <ul className="!m-0">
+            <li>상장 및 상금 200만원</li>
+          </ul>
+        </div>
+
+        <div className="mt-16">
+          <h5>최우수상 (3팀)</h5>
+          <ul className="!m-0">
+            <li>상장 및 각 상금 100만원</li>
+          </ul>
+        </div>
+
+        <div className="mt-16">
+          <h5>우수상 (6팀)</h5>
+          <ul className="!m-0">
+            <li>상장 및 각 상금 50만원</li>
+          </ul>
+        </div>
+
+        <div className="mt-16">
+          <h5>특별상 (4팀)</h5>
+          <ul className="!m-0">
+            <li>상장 및 상품</li>
+          </ul>
+        </div>
+
+        <h4>진행 일정</h4>
+        <h5>
+          모든 일정은 오프라인으로 진행되며, 상세 일정은 예고 없이 변경될 수
+          있습니다.
+        </h5>
+        <table className="timetable">
+          <tbody>
+            <tr>
+              <th>시간</th>
+              <th>일정</th>
+              <th>비고</th>
+            </tr>
+            <tr>
+              <td className="time">18:00</td>
+              <td>개회식 및 개발 시작</td>
+              <td> </td>
+            </tr>
+            <tr>
+              <td className="time">19:00</td>
+              <td>석식</td>
+              <td> </td>
+            </tr>
+            <tr>
+              <td className="time">21:00</td>
+              <td>현장 접수 마감</td>
+              <td> </td>
+            </tr>
+            <tr>
+              <td className="time">23:00</td>
+              <td>야식</td>
+              <td> </td>
+            </tr>
+            <tr>
+              <td className="time">06:30</td>
+              <td>조식</td>
+              <td> </td>
+            </tr>
+            <tr>
+              <td className="time">08:30</td>
+              <td>발표</td>
+              <td> </td>
+            </tr>
+            <tr>
+              <td className="time">11:40</td>
+              <td>심사</td>
+              <td> </td>
+            </tr>
+            <tr>
+              <td className="time">12:00</td>
+              <td>시상 및 폐회</td>
+              <td> </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <h4>주최</h4>
+        <p>
+          <a
+            href="https://swedu.khu.ac.kr/"
+            target="_blank"
+            className="favicon"
+          >
+            <img
+              src="https://khlug.org/images/khu.png"
+              className="favicon"
+              alt=""
+            />{" "}
+            경희대학교 <strong>AI·SW 교육단</strong>
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://swuniv.kyonggi.ac.kr/"
+            target="_blank"
+            className="favicon"
+          >
+            <img
+              src="https://cdn.khlug.org/images/kgu-univ-logo.png"
+              className="favicon"
+              alt=""
+            />{" "}
+            경기대학교 <strong>SW중심대학사업단</strong>
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://swcu.dankook.ac.kr/"
+            target="_blank"
+            className="favicon"
+          >
+            <img
+              src="https://cdn.khlug.org/images/dku-univ-logo.png"
+              className="favicon"
+              alt=""
+            />{" "}
+            단국대학교 <strong>SW중심대학</strong>
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://www.ajou.ac.kr/sw/"
+            target="_blank"
+            className="favicon"
+          >
+            <img
+              src="https://cdn.khlug.org/images/ajou-univ.png"
+              className="favicon"
+              alt=""
+            />{" "}
+            아주대학교 <strong>SW중심대학사업</strong>
+          </a>
+        </p>
+
+        <h4>주관</h4>
+        <p>
+          <a href="https://khlug.org/" target="_blank" className="favicon">
+            <img
+              src="https://khlug.org/images/favicon.gif"
+              className="favicon"
+              alt=""
+            />{" "}
+            경희대학교 중앙IT동아리 <strong>쿠러그</strong>
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://swedu.khu.ac.kr/"
+            target="_blank"
+            className="favicon"
+          >
+            <img
+              src="https://khlug.org/images/khu.png"
+              className="favicon"
+              alt=""
+            />{" "}
+            경희대학교 <strong>SW중심대학사업단</strong>
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://imcoss.khu.ac.kr/"
+            target="_blank"
+            className="favicon"
+          >
+            <img
+              src="https://khlug.org/images/khu.png"
+              className="favicon"
+              alt=""
+            />{" "}
+            경희대학교 <strong>실감미디어 혁신융합대학 사업단</strong>
+          </a>
+        </p>
+
+        <h4>문의</h4>
+        <ul className="!m-0">
+          <li>
+            쿠러그 공식 이메일:{" "}
+            <a href="mailto:we_are@khlug.org">we_are@khlug.org</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
