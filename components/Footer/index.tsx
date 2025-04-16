c12f55d87a3457ba64345d60e5b5cc613f6a7e64
Coalery <doralife12@gmail.com>
2025-04-16 21:49:55 +0900

    feat: footer 리팩토링 및 css 분리

---
+++ components/Footer/index.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+import styles from "./style.module.css";
+
+export function Footer() {
+  return (
+    <div className={styles.footer}>
+      <a href="http://khu.ac.kr/" target="_blank">
+        <Image
+          src="https://khlug.org/images/khu.png"
+          width={20}
+          height={14}
+          alt="경희대학교 로고"
+          className="inline"
+        />{" "}
+        경희대학교
+      </a>
+      <a href="https://khlug.org/" target="_blank">
+        <Image
+          src="https://khlug.org/images/favicon_white.gif"
+          width={14}
+          height={14}
+          alt="쿠러그 로고"
+          className="inline"
+        />{" "}
+        쿠러그
+      </a>
+      <a href="/judge">
+        <Image
+          src="/images/favicon.png"
+          width={14}
+          height={14}
+          alt="쿠톤 로고"
+          className="inline"
+        />{" "}
+        khu
+        <b>thon</b>
+      </a>
+    </div>
+  );
+}
