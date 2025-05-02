1176ac312f3dad6570ad44ec7e76dda1a548e1ba
Coalery <doralife12@gmail.com>
2025-05-02 17:22:30 +0900

    feat: 팀 그룹 필터 추가

--- app/manager/teams/components/TeamSearchBar/index.tsx
+++ app/manager/teams/components/TeamSearchBar/index.tsx
@@ -1,27 +1,59 @@
-import { FormEvent, useState } from "react";
+import Button from "@khlug/components/Button";
+import Dropdown from "@khlug/components/Dropdown";
+import TextField from "@khlug/components/TextField";
+import { Group } from "@khlug/constant";
+import { FormEvent, useEffect, useState } from "react";
 
 export type SearchParams = {
   name?: string;
+  group?: Group;
 };
 
 type Props = {
   onSearch: (params: SearchParams) => void;
 };
 
+const ALL_GROUP = "ALL";
+type GroupSearchParams = Group | typeof ALL_GROUP;
+
 export default function TeamSearchBar({ onSearch }: Props) {
   const [keyword, setKeyword] = useState("");
+  const [group, setGroup] = useState<GroupSearchParams>(ALL_GROUP);
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     onSearch({
       name: !!keyword.length ? keyword : undefined,
+      group: group === ALL_GROUP ? undefined : group,
     });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
-      <button type="submit">검색</button>
+      <div className="flex gap-2">
+        <TextField
+          className="max-w-80 inline-block"
+          label="팀 이름"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <Dropdown
+          className="max-w-32 inline-block"
+          label="그룹"
+          onChange={(e) => setGroup(e.target.value as GroupSearchParams)}
+          value={group}
+        >
+          <option value={ALL_GROUP}>전체</option>
+          {Object.entries(Group).map(([key, value]) => (
+            <option key={key} value={value}>
+              {value}
+            </option>
+          ))}
+        </Dropdown>
+      </div>
+      <Button className="mt-2" formSubmit>
+        검색
+      </Button>
     </form>
   );
 }
