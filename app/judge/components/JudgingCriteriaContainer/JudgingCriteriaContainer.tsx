558fea676aae2db7e7b73b167e45a88f2b7cb595
Coalery <doralife12@gmail.com>
2025-05-08 02:22:23 +0900

    fix: 심사 기준 반영

--- app/judge/components/JudgingCriteriaContainer/JudgingCriteriaContainer.tsx
+++ app/judge/components/JudgingCriteriaContainer/JudgingCriteriaContainer.tsx
@@ -6,19 +6,34 @@ export default function JudgingCriteriaContainer() {
       <h4>심사 기준</h4>
       <ul className="!m-0">
         <li>
-          <strong>아이디어의 독창성</strong>: 얼마나 참신하게 문제를 해결했는가
+          <strong>아이디어의 독창성</strong>: 참신하게 문제를 해결했는가
         </li>
         <li>
-          <strong>아이디어의 실용도</strong>: 얼마나 쓸모가 있는가
+          <strong>아이디어의 효용성</strong>: 현실에 적용 가능성이 있는가
         </li>
         <li>
-          <strong>산출물의 기술력</strong>: 얼마나 최신/고급 기술을 사용했는가
+          <strong>방법의 적절성</strong>: 직접 정의한 문제를 최적의 방법으로
+          해결했는가
         </li>
         <li>
-          <strong>산출물의 디자인</strong>: 얼마나 아름다운가
+          <strong>산출물의 디자인</strong>: 사용하기 편하고 아름다운가
         </li>
         <li>
-          <strong>산출물의 완성도</strong>: 얼마나 결과물의 완전한가
+          <strong>산출물의 완성도</strong>: 마무리가 되었는가
+          <ul className="!m-0">
+            <li>
+              산출물을 <strong>시연하지 않은 경우</strong>에는 완성도 점수를
+              0점으로 부여합니다.
+            </li>
+            <li>
+              데이터 분석에서는 분석 결과를 제시해야 하는 등, 설계가 아닌{" "}
+              <strong>구체적인 결과</strong>가 보여져야 합니다.
+            </li>
+            <li>
+              미완성 시연은 완성도 점수가 낮아질 수는 있더라도 시연한 것으로
+              판단합니다.
+            </li>
+          </ul>
         </li>
         <li>
           <strong>주제와의 적합성</strong>: (주제에 부합하는 산출물이 아닌 경우
