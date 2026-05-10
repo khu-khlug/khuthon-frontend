fcfb2efbb64ae362f19ac65c4575b573c13f3071
Coalery <doralife12@gmail.com>
2026-05-10 17:11:01 +0900

    feat: 수상 정보 추가

--- app/about/2026/page.tsx
+++ app/about/2026/page.tsx
@@ -19,9 +19,7 @@ export default function _2026About() {
 
         <h4>주제</h4>
         <Subject />
-        <p>
-          행사 당일 현장에서 3개의 가주제 중 1개의 주제로 확정됩니다.
-        </p>
+        <p>행사 당일 현장에서 3개의 가주제 중 1개의 주제로 확정됩니다.</p>
         <p>
           다양한 분야로 도전할 수 있습니다.
           <br />
@@ -77,6 +75,18 @@ export default function _2026About() {
           <ul className="!m-0">
             <li>상장 및 상금 200만원</li>
           </ul>
+          <div className="list" style={{ marginBottom: "50px" }}>
+            <ul>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">맥도날드좋아해요</div>
+                <div className="idea">
+                  장인과 창작자를 잇는 전통문화 마켓 플레이스
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+            </ul>
+          </div>
         </div>
 
         <div className="mt-16">
@@ -84,6 +94,37 @@ export default function _2026About() {
           <ul className="!m-0">
             <li>상장 및 각 상금 100만원</li>
           </ul>
+          <div className="list" style={{ marginBottom: "50px" }}>
+            <ul>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">소음</div>
+                <div className="idea">
+                  검색·알고리즘 없이, 모르는 문화가 다가오고 받는 자가 다음
+                  매개자가 되는 회로.
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">큐리언트</div>
+                <div className="idea">
+                  섬 넘네 : 알고리즘 밖으로 떠나는 취향 탐험 서비스
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">최고의 공룡 스피노사우루스</div>
+                <div className="idea">
+                  인기 지역만 소비되는 구조를 넘어, 사용자의 선택을 기반으로
+                  숨겨진 지역 문화를 연결하고 탐험하게 만드는 참여형 문화 발견
+                  플랫폼
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+            </ul>
+          </div>
         </div>
 
         <div className="mt-16">
@@ -91,13 +132,116 @@ export default function _2026About() {
           <ul className="!m-0">
             <li>상장 및 각 상금 50만원</li>
           </ul>
+          <div className="list" style={{ marginBottom: "50px" }}>
+            <ul>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">Six Sense</div>
+                <div className="idea">
+                  편향된 알고리즘을 극복하고 디지털 시대의 B-Side 음악을
+                  발굴하기위해서 Side-B 프로젝트가 탄생했습니다.
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">AI-Powered Rangers</div>
+                <div className="idea">
+                  TrenDo는 빠르게 소비되고 사라지는 대중문화를, 직접 참여하고
+                  함께 경험하는 문화로 전환하는 플랫폼이다.
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">쿠카프</div>
+                <div className="idea">
+                  오디오 레퍼런스 기반 국악 샘플 탐색 도구, DigGak(디각)
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">어쩌려고 그런 말을 해</div>
+                <div className="idea">
+                  독립영화의 지속적인 창작을 돕는 플랫폼 - MOV:ON
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">취준즈</div>
+                <div className="idea">
+                  이어줄게 : 3D 체험과 AI 창작으로 전통문화의 단절을 잇다
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">KHUREKA</div>
+                <div className="idea">
+                  대중문화 접근 불평등을 해소하는 간편 예매 플랫폼
+                  &quot;도담&quot;
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+            </ul>
+          </div>
         </div>
 
         <div className="mt-16">
-          <h5>특별상 (4팀)</h5>
+          <h5>인기상 (2팀)</h5>
           <ul className="!m-0">
             <li>상장 및 상품</li>
           </ul>
+          <div className="list" style={{ marginBottom: "50px" }}>
+            <ul>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">은수 없는 은수팀</div>
+                <div className="idea">
+                  Spotify Chess : 음원 시장의 승자독식을 뒤집는 스포티파이 연동
+                  역밸런싱 오토배틀러
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">멋진돈코츠</div>
+                <div className="idea">
+                  ReBurn : K-pop 앨범 소비 구조를 디지털 인터렉티브 앨범
+                  경험으로 재설계하다.
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="mt-16">
+          <h5>격려상 (2팀)</h5>
+          <ul className="!m-0">
+            <li>상장 및 상품</li>
+          </ul>
+          <div className="list" style={{ marginBottom: "50px" }}>
+            <ul>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">코딩햄</div>
+                <div className="idea">
+                  취향 책장 : 나의 감상을 바탕으로 취향을 확립하고, 이를 새로운
+                  문화 경험으로 연결하는 AI 감상 아카이브
+                </div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+              <li className="item" style={{ paddingLeft: "60px" }}>
+                <div className="name">FEPSI</div>
+                <div className="idea">analyze : AI 기반 광고 분석 솔루션</div>
+                <div className="member"></div>
+                <div className="clear"></div>
+              </li>
+            </ul>
+          </div>
         </div>
 
         <h4>진행 일정</h4>
